ff8287c2d68fa7751caa5299b8de16a5af80c46b
Jan Wijffels <jwijffels@bnosac.be>
2015-10-22 17:59:36 +0200

    Add BelgiumPopulation R package

---
+++ BelgiumPopulation/R/pkg.R
@@ -0,0 +1,10 @@
+#' Belgium population statistics available as open data 
+#' 
+#' More information: http://statbel.fgov.be/nl/statistieken/opendata/home
+#'
+#' @name BelgiumPopulation-package 
+#' @aliases ETLUtils ETLUtils-package
+#' @docType package 
+#' @examples
+#' data(tf_births)
+NULL
---
+++ BelgiumPopulation/R/population.R
@@ -0,0 +1,6 @@
+
+#' @name tf_births
+#' @title tf_births
+#' @description tf_births
+#' @docType data
+NULL
