49d0119186356bfcda796cc6419ab889ae5cc47f
Jan Wijffels <jwijffels@bnosac.be>
2015-10-27 15:17:26 +0100

    And lifting off - launch!

---
+++ BelgiumStatistics/R/pkg.R
@@ -0,0 +1,11 @@
+#' Belgium population statistics available as open data 
+#' 
+#' More information: http://statbel.fgov.be/nl/statistieken/opendata/home
+#'
+#' @name BelgiumPopulation-package 
+#' @aliases BelgiumPopulation BelgiumPopulation-package
+#' @docType package 
+#' @importFrom data.table setDF fread
+#' @examples
+#' data(tf_births)
+NULL
