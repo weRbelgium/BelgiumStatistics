8060b909b82fd4535ac0c19ccb0e8ceebad7c7c2
Jan Wijffels <jwijffels@bnosac.be>
2015-11-03 17:40:18 +0100

    Add landuse, places, points, railways, roads, waterways

---
+++ BelgiumMaps/BelgiumMapsOpenStreetMap/R/datasets_geofabrik.R
@@ -0,0 +1,83 @@
+#' @name BE_OSM_PLACES
+#' @title BE_OSM_PLACES
+#' @description Geofabrik shapefile places imported as: BE_OSM_PLACES. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_PLACES)
+#' str(as.data.frame(BE_OSM_PLACES))
+#' library(sp)
+#' plot(BE_OSM_PLACES)
+#' }
+NULL
+
+#' @name BE_OSM_POINTS
+#' @title BE_OSM_POINTS
+#' @description Geofabrik shapefile points imported as: BE_OSM_POINTS. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_POINTS)
+#' str(as.data.frame(BE_OSM_POINTS))
+#' library(sp)
+#' plot(BE_OSM_POINTS)
+#' }
+NULL
+
+#' @name BE_OSM_RAILWAYS
+#' @title BE_OSM_RAILWAYS
+#' @description Geofabrik shapefile railways imported as: BE_OSM_RAILWAYS. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_RAILWAYS)
+#' str(as.data.frame(BE_OSM_RAILWAYS))
+#' library(sp)
+#' plot(BE_OSM_RAILWAYS)
+#' }
+NULL
+
+#' @name BE_OSM_ROADS
+#' @title BE_OSM_ROADS
+#' @description Geofabrik shapefile roads imported as: BE_OSM_ROADS. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_ROADS)
+#' str(as.data.frame(BE_OSM_ROADS))
+#' library(sp)
+#' plot(BE_OSM_ROADS)
+#' }
+NULL
+
+#' @name BE_OSM_WATERWAYS
+#' @title BE_OSM_WATERWAYS
+#' @description Geofabrik shapefile waterways imported as: BE_OSM_WATERWAYS. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_WATERWAYS)
+#' str(as.data.frame(BE_OSM_WATERWAYS))
+#' library(sp)
+#' plot(BE_OSM_WATERWAYS)
+#' }
+NULL
+
+#' @name BE_OSM_LANDUSE
+#' @title BE_OSM_LANDUSE
+#' @description Geofabrik shapefile landuse imported as: BE_OSM_LANDUSE. More information about this data can be found in the inst/docs folder
+#' @docType data
+#' @references \url{https://taginfo.openstreetmap.org/}, \url{http://wiki.openstreetmap.org/wiki/Tags}, \url{http://download.geofabrik.de/europe/belgium-latest.shp.zip}, \url{http://download.geofabrik.de/europe/belgium.html}
+#' @examples
+#' \dontrun{
+#' data(BE_OSM_LANDUSE)
+#' str(as.data.frame(BE_OSM_LANDUSE))
+#' library(sp)
+#' plot(BE_OSM_LANDUSE)
+#' }
+NULL
